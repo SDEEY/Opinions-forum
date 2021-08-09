0a2e6f692ab274005d4f8a9511134002aca2cea2
George <seeyouagainpesh@gmail.com>
2021-08-09 16:24:41 +0500

    signIn and signUp ready without REST API

--- src/Components/Routes/SignIn/SignIn.jsx
+++ src/Components/Routes/SignIn/SignIn.jsx
@@ -1,11 +1,33 @@
+import {useForm} from "react-hook-form";
+import s from "../SignUp/SignUp.module.css";
+
 function SignIn(){
+    const {register, formState: {errors}, handleSubmit} = useForm();
+
+    const onSubmit = data => console.log(data)
     return(
-        <div>
-            <h2>Sign in</h2>
-            <form>
-                <div><input placeholder='Login'/></div>
-                <div><input placeholder='Password'/></div>
-                <button>Submit</button>
+        <div className={s.form}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className={s.errorBlock}>
+                    <div>
+                        <label htmlFor='email'>Email</label>
+                        <input type='email' {...register("email", {required: true})} />
+                    </div>
+                    <div className={s.error}>
+                        {errors.email && "Email is required"}
+                    </div>
+                </div>
+                <div className={s.errorBlock}>
+                    <div>
+                        <label htmlFor='password'>Password</label>
+                        <input type='password' {...register("password", {required: true, minLength: 8})} />
+                    </div>
+                    <div className={s.error}>
+                        {errors.password && "Password is required, min length 8 symbols"}
+                    </div>
+                </div>
+
+                <button className={s.button} type="submit">Sign in</button>
             </form>
         </div>
     )
--- src/Components/Routes/SignUp/SignUp.jsx
+++ src/Components/Routes/SignUp/SignUp.jsx
@@ -1,16 +1,51 @@
-function SignUp(){
-    return(
-        <div>
-            <h2>Sign up</h2>
-            <form>
-                <div><input placeholder='Firs name'/></div>
-                <div><input placeholder='Last name'/></div>
-                <div><input placeholder='Email'/></div>
-                <div><input placeholder='Password'/></div>
-                <button>Submit</button>
+import {useForm} from "react-hook-form";
+import s from './SignUp.module.css'
+
+function SignUp() {
+    const {register, formState: {errors}, handleSubmit} = useForm();
+
+    const onSubmit = data => console.log(data)
+
+    console.log(errors)
+    return (
+        <div className={s.form}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className={s.errorBlock}>
+                    <div>
+                        <label htmlFor='username'>Username</label>
+                        <input type='text' {...register("username", {
+                            required: true,
+                            maxLength: 15,
+                            pattern: /^[A-Za-z0-9]+$/i
+                        })} />
+                    </div>
+                    <div className={s.error}>
+                        {errors.username && "Username is required, max length 15 symbols"}
+                    </div>
+                </div>
+                <div className={s.errorBlock}>
+                    <div>
+                        <label htmlFor='email'>Email</label>
+                        <input type='email' {...register("email", {required: true})} />
+                    </div>
+                    <div className={s.error}>
+                        {errors.email && "Email is required"}
+                    </div>
+                </div>
+                <div className={s.errorBlock}>
+                    <div>
+                        <label htmlFor='password'>Password</label>
+                        <input type='password' {...register("password", {required: true, minLength: 8})} />
+                    </div>
+                    <div className={s.error}>
+                        {errors.password && "Password is required, min length 8 symbols"}
+                    </div>
+                </div>
+
+                <button className={s.button} type="submit">Sign up</button>
             </form>
         </div>
-    )
+    );
 }
 
 export default SignUp
